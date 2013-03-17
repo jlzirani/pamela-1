aaf88f861dcfb34cb569d6c92237066edd64f809
C4ptainCrunch <nikita.marchant@gmail.com>
2013-03-17 14:42:04 +0100

    [init] new modular version of pamela. will respect privacy and will use a lot of fun libs like 0mq, circus, sh (pbs) and django. why not buy pamela.io ?

---
+++ client/spy.py
@@ -0,0 +1,68 @@
+from sh import arp_scan,arp
+from netaddr import *
+import zmq, time, json
+
+INTERFACE = 'en0'
+
+def prescan():
+    '''Function to execute before dumping mac adresses
+    eg : nmap or broadcast ping'''
+    pass
+
+def postscan(macs):
+    '''Function to execute after dumping mac adresses
+    eg : clean unwanted mac adresses fo hiding routers and switches
+    or fix encoding glitches'''
+    for mac in macs:
+        try:
+            EUI(mac)
+            IPAddress(macs[mac])
+        except:
+            del macs[mac]
+    return macs
+
+def scan(pre=False):
+    '''Dump mac adresses and ips.
+    Returns a dict {mac:ip,...}'''
+    if pre:
+        prescan()
+    return postscan(arp_dump())
+
+def arp_dump():
+    '''[Internal] Uses arp -a to dump mac adresses
+    Returns a dict {mac:ip,...}'''
+    dump = arp("-a")
+    lines = dump.strip().split('\n')
+    ret = {}
+    for line in lines:
+        split = line.strip().split(' ')
+        ip = split[1][1:-1]
+        mac = split[3]
+        if split[4] == 'on':
+            iface = split[5]
+        else:
+            iface = split[6]
+        if iface == INTERFACE:
+            ret[mac] = ip
+    return ret
+
+
+def arp_scan_dump():
+    '''[Internal] Uses arp-scan to dump mac adresses
+    Returns a dict {mac:ip,...}'''
+    raise NotImplementedError
+    arp_scan("-l", "-I", INTERFACE)
+
+def main():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://127.0.0.1:5000")
+    while True:
+        socket.send(json.dumps(scan()))
+        print "Sending dict"
+        msg_in = socket.recv()
+        print msg_in
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main()
